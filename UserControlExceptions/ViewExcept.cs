7019da6f9d2bfd883be2b3997dd5e3393cced124
Anthony Wilkey <groovykool@gmail.com>
2019-11-08 13:11:04 -0600

    Cleanup.  Baseline

---
+++ UserControlExceptions/ViewExcept.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UserControlExceptions
+{
+  public class ViewExcept : UserControl
+  {
+    
+    public ViewExcept()
+    {
+      try
+      {
+        var TB = new TextBlock
+        {
+          Text = "This is a test."
+        };
+        Content = TB;
+        Loaded += ViewExcept_Loaded;
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine($"Caught in Constructor{ex}");
+      }
+    }
+
+    private void ViewExcept_Loaded(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        ExceptMethod();
+      }
+      catch (Exception ex)
+      {
+        //Debug.WriteLine($"Caught in ViewExcept_Loaded::{ex.Message}");
+        throw new Exception($"Rethrown::{ex.Message}");
+      }
+    }
+
+    public void ExceptMethod()
+    {
+      throw new Exception("Test exception was thrown");
+    } 
+  }
+}
